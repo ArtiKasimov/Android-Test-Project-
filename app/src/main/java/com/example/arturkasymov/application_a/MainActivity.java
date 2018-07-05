04dbec5909664b0cecd3eaaf8e8dced8e6b0c14a
Muliar Misha <muliar.misha@gmail.com>
2018-07-05 15:15:11 +0300

    Tabs

--- app/src/main/java/com/example/arturkasymov/application_a/MainActivity.java
+++ app/src/main/java/com/example/arturkasymov/application_a/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.arturkasymov.application_a;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import  android.support.design.widget.TabLayout;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout simpleFrameLayout;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleFrameLayout = (FrameLayout) findViewById(R.id.simpleFrameLayout);
+        tabLayout = (TabLayout) findViewById(R.id.simpleTabLayout);
+
+        TabLayout.Tab firstTab = tabLayout.newTab();
+        firstTab.setText("First");
+        tabLayout.addTab(firstTab);
+
+        TabLayout.Tab secondTab = tabLayout.newTab();
+        secondTab.setText("Second");
+        tabLayout.addTab(secondTab);
+
+        Fragment fragment = new FirstFragment();
+        changeFragment(fragment);
+
+        // perform setOnTabSelectedListener event on TabLayout (deprecated)
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // get the current selected tab's position and replace the fragment accordingly
+                Fragment fragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new FirstFragment();
+                        break;
+                    case 1:
+                        fragment = new SecondFragment();
+                        break;
+                }
+                changeFragment(fragment);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public void changeFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.simpleFrameLayout, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
     }
 }
