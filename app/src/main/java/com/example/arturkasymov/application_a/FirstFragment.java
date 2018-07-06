1abd9d2b708e71913579f92e8f14f138814fe1c4
Artur Kasymov <arti.kasim@gmail.com>
2018-07-06 17:22:49 +0300

    intent to B

--- app/src/main/java/com/example/arturkasymov/application_a/FirstFragment.java
+++ app/src/main/java/com/example/arturkasymov/application_a/FirstFragment.java
@@ -1,15 +1,24 @@
 package com.example.arturkasymov.application_a;
 
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class FirstFragment extends Fragment {
+
+    private String image_URL;
+    private final String EXTRA_IMAGE_URL = "com.example.arturkasymov.application_a.image_URL";
+    public static final int REQUEST_CODE_APPLICATION_B = 7;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -27,15 +36,36 @@ public class FirstFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.first_fragment, container, false);
 
-        final Button button = view.findViewById(R.id.button);
+        final Button button = view.findViewById(R.id.buttonOK);
         final EditText editText = view.findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                image_URL=s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 DBHandler dbHandler = new DBHandler(getContext());
 
-               // Re_cord record = new Re_cord(editText.getText().toString(), 1, 1);
-               // dbHandler.addRe_cord(record);
-                //editText.setText(dbHandler.getRe_cord(0).getReference().toString());
+                Intent i = new Intent();
+                i.setComponent(new ComponentName("com.example.arturkasymov.application_b",
+                        "com.example.arturkasymov.application_b.MainActivity"));
+                i.putExtra( EXTRA_IMAGE_URL, image_URL);
+                startActivity(i);
+
 
             }
         });
