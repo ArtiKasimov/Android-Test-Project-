4cd649d308b8cedec8d488a1a42e03cea740f8f0
Muliar Misha <muliar.misha@gmail.com>
2018-07-06 12:19:10 +0300

    improving DB

--- app/src/main/java/com/example/arturkasymov/application_a/SecondFragment.java
+++ app/src/main/java/com/example/arturkasymov/application_a/SecondFragment.java
@@ -20,13 +20,20 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.second_fragment, container, false);
+        DBHandler dbHandler = new DBHandler(getContext());
 
-        ArrayList<String> references = new ArrayList<>();
-        references.add("first reference");
-        references.add("second reference");
-        references.add("iOS third reference");
-        references.add("Mac OSX fourth reference");
+        dbHandler.getAllRe_cords();
 
+        //ArrayList<Re_cord> allRecords = (ArrayList) (dbHandler.getAllRe_cords());
+
+        // it's needed while we haven't custom adapter
+  /*      ArrayList<String> references = new ArrayList<String>();
+        for (Re_cord temp: allRecords){
+            references.add(temp.getReference());
+        }
+        // it's end
+
+        // it's must be improved
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                     getContext(),
                     android.R.layout.simple_list_item_1,
@@ -34,6 +41,8 @@ public class SecondFragment extends Fragment {
 
         ListView listView = view.findViewById(R.id.listview);
         listView.setAdapter(adapter);
+        // it's end
+*/
         // Inflate the layout for this fragment
         return view;
     }
